3c2737260f33f8437db989fe24c2d0638595e9cd
lucas-garcon <32601336+lucas-garcon@users.noreply.github.com>
2018-07-31 20:49:29 -0500

    redo
    yup it's all being redone

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
-// Get Modal elements
+$(function() {
+  $('#animateBtn').click(function() {
+    animate('#cover', 'fadeOutLeft');
+    setTimeout(function() {
+      $('#cover').css('visibility', 'hidden');
+    }, 950);
 
-var modal = $('#bodmodal');
-var closeBtn = $('.closeBtn')
+    setTimeout(function() {
+    window.location = '/pages/pg2.html';
+  },900);
 
-modalBtn.
+  return false;
+  });
+
+
+  //custom animation
+  function animate(element, animation){
+    $(element).addClass('animated ' +animation);
+    var wait = setTimeout(function() {
+      $(element).removeClass('animated ' +animation);
+    }, 1000);
+  }
+})
+
+
+
+$(function() {
+    $('#pg2btn').click(function() {
+      animate('#offers1', 'fadeOutLeft');
+      setTimeout(function() {
+        $('.offers-1').css('visibility', 'hidden');
+      }, 950);
+
+      setTimeout(function() {
+      window.location = '/pages/proshop.html';
+    },900);
+
+    return false;
+    });
+
+    //custom animation
+    function animate(element, animation){
+      $(element).addClass('animated ' +animation);
+      var wait = setTimeout(function() {
+        $(element).removeClass('animated ' +animation);
+      }, 1000);
+    }
+  })
